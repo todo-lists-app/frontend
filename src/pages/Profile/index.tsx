95520b1497223f997a327169624090bf0a1cc196
Keloran <keloran@develbox.info>
2023-07-17 11:56:27 +0100

    see if actions work

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,5 +1,5 @@
 import React, {FC, useEffect, useState} from "react";
-import {Box} from "dracula-ui";
+import {Box, Card, Heading} from "dracula-ui";
 import {TodoItem, TodoList} from "../../lib/todo";
 import {useAuth} from "react-oidc-context";
 import {
@@ -17,8 +17,16 @@ export const Profile: FC = () => {
   console.log("profile", auth?.user?.profile)
 
   return (
-    <div>
-      <h1>Profile!</h1>
-    </div>
+    <>
+      <Box>
+        <Heading>Profile</Heading>
+        <Box p={"sm"} borderColor={"purple"}>
+          <p>Username: {auth?.user?.profile?.preferred_username}</p>
+          <p>First name: {auth?.user?.profile?.given_name}</p>
+          <p>Last name: {auth?.user?.profile?.family_name}</p>
+          <p>Email: {auth?.user?.profile?.email}</p>
+        </Box>
+      </Box>
+    </>
   );
 }
